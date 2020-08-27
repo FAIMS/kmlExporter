26eb9ddaccffb6160d0b9eb1665fd154b8ebe9bc
Brian Ballsun-Stanton <brian@fedarch.org>
2020-08-27 16:11:10 +1000

    hoping for an rbenv install

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@ sudo apt-get update
 sudo apt-get install python spatialite-bin libimage-exiftool-perl libspatialite-dev python-pip jhead python-numpy -y
 
 sudo pip install python-magic fastkml shapely ordereddict lxml numpy
+export PATH="$HOME/.rbenv/bin:$PATH" 
+eval "$(rbenv init -)"
+
+rbenv install 2.5.0
 
 if lsb_release -d -s | grep -q 16.04; then
 	sudo apt-get install libsqlite3-mod-spatialite
