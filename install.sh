56aa06c49a6b2d885bf855ea3546e84504bef880
Brian Ballsun-Stanton <brian@fedarch.org>
2020-08-27 16:20:19 +1000

    gem install

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 rbenv install 2.5.0
-
+rbenv local 2.5.0
+gem install sqlite3 antlr3
 if lsb_release -d -s | grep -q 16.04; then
 	sudo apt-get install libsqlite3-mod-spatialite
 fi
