acba1001184f78ffbb59c229f6ce33a06eca6046
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2021-04-16 11:56:14 +1000

    gemfile added, ruby to 2.5.6

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ sudo pip install python-magic fastkml shapely ordereddict lxml numpy
 export PATH="$HOME/.rbenv/bin:$PATH" 
 eval "$(rbenv init -)"
 
-rbenv install 2.5.0
+rbenv install 2.5.6
+#rbenv local 2.5.6
+bundler install
 
 if lsb_release -d -s | grep -q 16.04; then
 	sudo apt-get install libsqlite3-mod-spatialite
